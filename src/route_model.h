76deb8d16a132e11fac4f2d4ca87da4ea65ad93a
Vijayasri Iyer <thisisvij98@gmail.com>
2019-05-21 17:15:04 +0530

    Initialized routemodel variables

--- src/route_model.h
+++ src/route_model.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
 
 #include <limits>
 #include <cmath>
@@ -12,21 +13,29 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
+        Node* parent = NULL;
+        float h_value = std::numeric_limits<int>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node*> neighbors;
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+
       private:
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
     };
-    
+
     // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
+    RouteModel(const std::vector<std::byte> &xml);
+    auto &SNodes() {return m_Nodes;}
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+
 
   private:
     // Add private RouteModel variables and methods here.
-
+    std::vector<Node> m_Nodes;
 };
+
+#endif
