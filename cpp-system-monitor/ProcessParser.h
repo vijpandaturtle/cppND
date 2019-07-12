f2f33de569a8e2ca5dc8dacaf137d62e8dba9ca6
Vijayasri Iyer <thisisvij98@gmail.com>
2019-07-12 14:58:48 +0530

    Defined getVmSize()

--- cpp-system-monitor/ProcessParser.h
+++ cpp-system-monitor/ProcessParser.h
@@ -16,6 +16,7 @@
 #include <dirent.h>
 #include <time.h>
 #include <unistd.h>
+#include "util.h"
 #include "constants.h"
 
 
@@ -23,14 +24,14 @@ using namespace std;
 
 class ProcessParser{
 private:
-    std::ifstream stream;
+    ifstream stream;
     public:
     static string getCmd(string pid);
     static vector<string> getPidList();
-    static std::string getVmSize(string pid);
-    static std::string getCpuPercent(string pid);
+    static string getVmSize(string pid);
+    static string getCpuPercent(string pid);
     static long int getSysUpTime();
-    static std::string getProcUpTime(string pid);
+    static string getProcUpTime(string pid);
     static string getProcUser(string pid);
     static vector<string> getSysCpuPercent(string coreNumber = "");
     static float getSysRamPercent();
@@ -45,3 +46,36 @@ private:
 };
 
 // TODO: Define all of the above functions below:
+string ProcessParser::getCmd(string pid) {
+  string line;
+  //Declaring search attribute for file
+  string name = "Cmd";
+  string value;
+}
+
+vector<string> ProcessParser::getPidList() {
+
+}
+
+string ProcessParser::getVmSize(string pid) {
+  string line;
+  //Declaring search attribute for file
+  string name = "VmData";
+  string value;
+  float result;
+  //Opening stream for specific result
+  ifstream stream = Util::getStream((Path::basePath() + pid + Path.statusPath));
+  while(std::getline(stream, line)) {
+    //Searching line by line
+    if(line.compare(0, name.size.name(), name) == 0) {
+      //slicing string line on ws for values using sstream
+      istringstream buff(line);
+      istream_iterator<string> beg(buf), end;
+      vector<string> values(beg, end);
+      //conversion from kb to GB
+      result = (stof(values[1])/float(1024));
+      break;
+    }
+  };
+  return to_string(result);
+}
