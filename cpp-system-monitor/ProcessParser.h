1ebf3c7461734492a515e7f62a5efd97311da1af
Vijayasri Iyer <thisisvij98@gmail.com>
2019-07-29 10:53:50 +0530

    Added getPidList function

--- cpp-system-monitor/ProcessParser.h
+++ cpp-system-monitor/ProcessParser.h
@@ -158,3 +158,26 @@ string ProcessParser::getProcUser() {
   }
   return "";
 }
+
+vector<string> ProcessParser::getPidList() {
+  DIR *dir;
+  //return a PID list from procs
+  vector<string> container;
+  if(!(dir = opendir("/proc")))
+      throw std::runtime_error(std::strerror(errno));
+
+  while (dirent* dirp = readdir(dir)) {
+    //is this a directory
+    if(dirp->d_type != DT_DIR)
+        continue;
+    //is every character of the name a digit
+    if(all_of(dirp->d_name, dirp->d_name + std::strlen(dirp->d_name), [](char c){return std::isdigit(c); })) {
+      container.push_back(dirp->d_name);
+    }
+  }
+
+  //Validating process of directory closing
+  if(closeddir(dir))
+      throw std::runtime_error(std::strerror(errno));
+  return container;
+}
