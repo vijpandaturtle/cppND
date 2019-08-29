c0c1c9b5846fc71632210bd5168899851c7e7a65
Vijayasri Iyer <thisisvij98@gmail.com>
2019-08-30 00:07:02 +0530

    Added routeplanner constructor

--- cpp-route-planner/src/route_planner.h
+++ cpp-route-planner/src/route_planner.h
@@ -10,11 +10,14 @@
 class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
+    float GetDistance() const {return distance;}
 
   private:
-    // Add private variables or methods declarations here.
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
     RouteModel &m_Model;
+    RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+    float distance;
 };
 
 #endif
